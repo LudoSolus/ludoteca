f7643d6a04c51e86ffffbbda6a0b0ac12fd4abef
Felipe Scalco <fscalco7@gmail.com>
2025-10-03 10:46:11 -0300

    font awesome

---
+++ frontend/web/src/lib/components/atoms/Button.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { IconDefinition } from '@fortawesome/free-brands-svg-icons';
+	import Fa from 'svelte-fa';
+
+	export let text: string;
+	export let onClick: () => void;
+	export let leftIcon: IconDefinition | null = null;
+	export let rightIcon: IconDefinition | null = null;
+</script>
+
+<button
+	class="flex items-center gap-2 rounded-md border-2 border-black px-6 py-1"
+	on:click={onClick}
+>
+	{#if leftIcon}
+		<Fa icon={leftIcon} />
+	{/if}
+	{text}
+	{#if rightIcon}
+		<Fa icon={rightIcon} />
+	{/if}
+</button>
+
+<style>
+	button {
+		background-color: var(--primary-color);
+		font-weight: 600;
+		box-shadow: rgba(0, 0, 0, 0.5) 0px 4px 4px;
+		cursor: pointer;
+	}
+
+	button:hover {
+		background-color: var(--hover-color);
+	}
+
+	button:active {
+		background-color: var(--active-color);
+	}
+</style>
---
+++ frontend/web/src/routes/components/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Button from '$lib/components/atoms/Button.svelte';
+	import { faArrowLeftLong, faArrowRight, faTrash } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="mt-6 flex flex-col items-center gap-4">
+	<Button text={'Entrar'} onClick={() => {}} />
+	<Button text={'Entrar'} onClick={() => {}} leftIcon={faTrash} />
+	<Button text={'Entrar'} onClick={() => {}} rightIcon={faArrowRight} />
+</div>
+
+<style>
+</style>
