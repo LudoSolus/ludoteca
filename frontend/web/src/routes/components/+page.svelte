a963217085434cd20aa80a60372b01973dcaf9ad
Felipe Scalco <fscalco7@gmail.com>
2025-10-06 18:26:42 -0300

    input

--- frontend/web/src/routes/components/+page.svelte
+++ frontend/web/src/routes/components/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Button from '$lib/components/atoms/Button.svelte';
+	import Input from '$lib/components/atoms/Input.svelte';
 	import { faArrowLeftLong, faArrowRight, faTrash } from '@fortawesome/free-solid-svg-icons';
 </script>
 
@@ -7,6 +8,37 @@
 	<Button text={'Entrar'} onClick={() => {}} />
 	<Button text={'Entrar'} onClick={() => {}} leftIcon={faTrash} />
 	<Button text={'Entrar'} onClick={() => {}} rightIcon={faArrowRight} />
+	<Input
+		label={'Nome'}
+		placeholder={'João'}
+		width="300px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+	/>
+	<Input
+		label={'Nome'}
+		placeholder={'João'}
+		width="300px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+		error={'Nome inválido'}
+	/>
+	<Input
+		placeholder="Pesquisar"
+		width="500px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+	/>
+	<Input
+		width="300px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+		error={'Nome inválido'}
+	/>
 </div>
 
 <style>
