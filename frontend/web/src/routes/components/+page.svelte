7f47bb67a5b5f94a3bbd117ce6a440c49c441000
Felipe Scalco <87811238+Scalco7@users.noreply.github.com>
2025-10-06 18:29:42 -0300

    5-input (#25)
    * globals css
    
    * font awesome
    
    * input

---
+++ frontend/web/src/routes/components/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import Button from '$lib/components/atoms/Button.svelte';
+	import Input from '$lib/components/atoms/Input.svelte';
+	import { faArrowLeftLong, faArrowRight, faTrash } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="mt-6 flex flex-col items-center gap-4">
+	<Button text={'Entrar'} onClick={() => {}} />
+	<Button text={'Entrar'} onClick={() => {}} leftIcon={faTrash} />
+	<Button text={'Entrar'} onClick={() => {}} rightIcon={faArrowRight} />
+	<Input
+		label={'Nome'}
+		placeholder={'João'}
+		width="300px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+	/>
+	<Input
+		label={'Nome'}
+		placeholder={'João'}
+		width="300px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+		error={'Nome inválido'}
+	/>
+	<Input
+		placeholder="Pesquisar"
+		width="500px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+	/>
+	<Input
+		width="300px"
+		onInput={(value) => {
+			console.log(value);
+		}}
+		error={'Nome inválido'}
+	/>
+</div>
+
+<style>
+</style>
