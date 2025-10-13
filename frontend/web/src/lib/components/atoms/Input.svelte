9e79e3573d91c2598496632457ae454835c66f39
Felipe Scalco <fscalco7@gmail.com>
2025-10-13 11:54:40 -0300

    mask

--- frontend/web/src/lib/components/atoms/Input.svelte
+++ frontend/web/src/lib/components/atoms/Input.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
+	import { generateId } from '$lib/helpers/generate-id';
+	import { maska } from 'maska/svelte';
+	import type { MaskInputOptions } from 'maska';
+
+	type MaskType = 'phone' | 'cpf' | 'ra';
+
+	export let id: string = generateId(5);
 	export let type = 'text';
 	export let width: string = '100%';
+	export let height: string = 'fit-content';
 	export let value: string | null = null;
 	export let placeholder: string | null = null;
 	export let error: string | null = null;
 	export let label: string | null = null;
 	export let onInput: (value: string) => void;
+	export let mask: MaskType | null = null;
+
+	const maskOptions: MaskInputOptions | undefined = mask ? getMaskOptions() : undefined;
+
+	function getMaskOptions() {
+		switch (mask) {
+			case 'phone':
+				return {
+					mask: '(##) #####-####',
+					eager: false
+				};
+			case 'cpf':
+				return {
+					mask: '###.###.###-##',
+					eager: false
+				};
+			case 'ra':
+				return {
+					mask: '#######',
+					eager: false
+				};
+		}
+	}
 
 	function handleInput(event: any) {
 		value = event.target.value;
@@ -13,15 +44,16 @@
 	}
 </script>
 
-<div class="flex w-full flex-col" style="width: {width}; height: 100%;">
-	<label for="input" class="mb-1 font-bold text-black">
+<div class="flex flex-col" style="width: {width}; height: {height};">
+	<label for={id} class="mb-1 font-bold text-black">
 		{label}
 	</label>
 
 	<input
-		id="input"
+		{id}
 		{type}
 		bind:value
+		use:maska={maskOptions}
 		{placeholder}
 		on:input={handleInput}
 		class={`w-full rounded-md border px-3 py-2 focus:ring-2 focus:ring-yellow-400 focus:outline-none ${
