36440abeca4513d7d53aa9e618c7d020695853ea
RaphaeldosSantosSouza <164585988+RaphaeldosSantosSouza@users.noreply.github.com>
2025-11-05 22:09:26 -0300

    Criação de rota de detalhes de evento

---
+++ backend/src/main/java/com/projectLudoteca/ludoteca/query/controller/eventDetails/GetEventDetailsQuery.java
@@ -0,0 +1,6 @@
+package com.projectLudoteca.ludoteca.query.controller.eventDetails;
+
+import java.util.UUID;
+
+public record GetEventDetailsQuery(UUID id) {
+}
---
+++ backend/src/main/java/com/projectLudoteca/ludoteca/query/controller/eventDetails/GetEventDetailsView.java
@@ -0,0 +1,25 @@
+package com.projectLudoteca.ludoteca.query.controller.eventDetails;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record GetEventDetailsView(String name,
+                                  LocalDateTime startDate,
+                                  LocalDateTime finalDate,
+                                  String street,
+                                  String number,
+                                  String supplement,
+                                  String neighborhood,
+                                  String city,
+                                  String state,
+                                  String zipCode,
+                                  List<ListGamesView> listGames) {
+    public record ListGamesView(
+            String nameGame,
+            Boolean isAvailable,
+            String category,
+            Integer minPlayers,
+            Integer maxPlayers
+    ) {}
+}
