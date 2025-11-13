5f994c007e3f4ee0a13061e610a3d35493e4fd3c
Felipe Scalco <fscalco7@gmail.com>
2025-11-13 13:34:34 -0300

    feat [WEB]: rota inexistente

---
+++ frontend/web/src/routes/[...catchall]/+page.ts
@@ -0,0 +1,13 @@
+export const ssr = false;
+
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { authService } from '$lib/shared/stores/auth';
+
+export const load: PageLoad = () => {
+	if (!authService.isAuthenticated()) {
+		throw redirect(302, '/auth/login');
+	}
+
+	throw redirect(303, '/user/home');
+};
