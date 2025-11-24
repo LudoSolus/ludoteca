771c11cd53dbdc81236bed9bf681dc4564cf76a2
Felipe Scalco <fscalco7@gmail.com>
2025-11-24 10:11:34 -0300

    feat [WEB] update layout board games:

--- frontend/web/src/lib/components/templates/BoardGamesManager.svelte
+++ frontend/web/src/lib/components/templates/BoardGamesManager.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <main
-	class="flex h-full items-start justify-center lg:justify-between gap-20 overflow-y-auto px-1 py-7 sm:px-10 xl:px-15"
+	class="flex h-full items-start justify-center lg:justify-between gap-20 overflow-y-auto px-4 py-7 sm:px-10 xl:px-15"
 >
 	<section class="flex max-w-200 flex-1 flex-col items-center justify-center pt-0 lg:pt-20 gap-8">
 		<div class="flex w-full items-center justify-between">
@@ -24,11 +24,11 @@
 			<Button text="Criar" leftIcon={faPlus} onClick={() => {}} />
 		</div>
 
-		<div class="flex flex-col gap-8">
-			<div class="flex w-full max-w-150 items-center justify-start gap-5">
+		<div class="flex flex-col gap-4 sm:gap-8 w-full max-w-150">
+			<div class="flex w-full max-w-150 items-center justify-start gap-5 px-2 sm:px-4">
 				<SearchInput bind:value={searchInputValue} placeholder="Pesquisar..." />
 			</div>
-			<div class="flex max-h-76 w-fit flex-col gap-2 overflow-y-auto px-1 py-2 md:px-4">
+			<div class="flex max-h-76 w-full items-center flex-col gap-2 overflow-y-auto px-1 py-2 md:px-4">
 				{#if filteredGamesList.length > 0}
 					{#each filteredGamesList as game}
 						<BoardGame
@@ -40,7 +40,7 @@
 						/>
 					{/each}
 				{:else}
-					<p>Nenhum jogo encontrado.</p>
+					<p class="w-full">Nenhum jogo encontrado.</p>
 				{/if}
 			</div>
 		</div>
