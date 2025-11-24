66ba7d65700fc97eabbf27fcb62357f7f17deb80
Felipe Scalco <fscalco7@gmail.com>
2025-11-24 13:29:51 -0300

    feat [WEB]: game details admin page

--- frontend/web/src/lib/api/queries/board-games/list-board-games/list-board-games.interface.ts
+++ frontend/web/src/lib/api/queries/board-games/list-board-games/list-board-games.interface.ts
@@ -1,6 +1,8 @@
-import type { ECategory } from "$lib/shared/enums/category.enum";
+import type { ECategory } from '$lib/shared/enums/category.enum';
 
 export interface IBoardGame {
+	id: string;
+	barcode: string;
 	name: string;
 	category: ECategory;
 	minParticipants: number;
