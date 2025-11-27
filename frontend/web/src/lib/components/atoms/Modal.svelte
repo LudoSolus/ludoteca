44bc472077396712fb28f56324636db905b17fb6
Felipe Scalco <fscalco7@gmail.com>
2025-11-27 20:09:15 -0300

    fix [WEB]: modal refactor

--- frontend/web/src/lib/components/atoms/Modal.svelte
+++ frontend/web/src/lib/components/atoms/Modal.svelte
@@ -1,15 +1,66 @@
 <script lang="ts">
-	export let onClickOuside: () => void;
-</script>
+	import Button from './Button.svelte';
 
+	export let onClose: () => void;
+	export let onConfirm: () => void;
+	export let confirmButtonText: string
+	export let isLoading: boolean;
+</script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <div
-	class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center backdrop-blur-md z-11"
-	on:click={() => {
-		onClickOuside();
-	}}
+	class="absolute top-0 right-0 bottom-0 left-0 z-11 flex items-center justify-center backdrop-blur-md"
+	on:click={() => onClose()}
 >
-	<slot />
+	<div
+		class="modal-container flex max-w-9/10 flex-col items-center gap-2 rounded-2xl p-5 sm:gap-8 sm:px-15 sm:py-10 md:max-w-170"
+		on:click={() => event?.stopPropagation()}
+	>
+		<slot />
+		<div class="buttons-box flex w-full items-center justify-end gap-4">
+			<div class="button-box cancel-button">
+				<Button
+					text="Cancelar"
+					width="100%"
+					variant="secondary"
+					onClick={onClose}
+					disabled={isLoading}
+				/>
+			</div>
+
+			<div class="button-box register-button">
+				<Button text={confirmButtonText} width="100%" onClick={onConfirm} loading={isLoading} />
+			</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	.modal-container {
+		background: var(--modal-background-color);
+		box-shadow: 0 4px 4px 2px rgba(0, 0, 0, 0.5);
+	}
+
+	.buttons-box {
+		flex-direction: column-reverse;
+	}
+
+	.button-box {
+		width: 100%;
+	}
+
+	@media (min-width: 475px) {
+		.buttons-box {
+			flex-direction: row;
+		}
+
+		.cancel-button {
+			width: 130px;
+		}
+
+		.register-button {
+			width: 170px;
+		}
+	}
+</style>
--- frontend/web/src/lib/components/molecules/EventRegisterUserModal.svelte
+++ frontend/web/src/lib/components/molecules/EventRegisterUserModal.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { toast } from 'svoast';
-	import Button from '../atoms/Button.svelte';
 	import FormInput from '../atoms/FormInput.svelte';
 	import Modal from '../atoms/Modal.svelte';
 
@@ -34,74 +33,18 @@
 </script>
 
 {#if isOpen}
-	<!-- svelte-ignore a11y_no_static_element_interactions -->
-	<Modal onClickOuside={close}>
-		<!-- svelte-ignore a11y_click_events_have_key_events -->
-		<div
-			class="modal-container flex max-w-9/10 flex-col items-center gap-2 rounded-2xl p-5 sm:gap-8 sm:px-15 sm:py-10 md:max-w-170"
-			onclick={() => event?.stopPropagation()}
-		>
-			<div class="flex flex-col gap-6">
-				<p class="text-center text-lg font-medium sm:text-xl">
-					Digite o Id público do usuário que deseja registrar no evento
-				</p>
-				<FormInput
-					label={'Id Público'}
-					height="90px"
-					bind:value={publicUserIdInput}
-					onInput={(v) => {}}
-					isUppercase
-				/>
-			</div>
-			<div class="buttons-box flex w-full items-center justify-end gap-4">
-				<div class="button-box cancel-button">
-					<Button
-						text="Cancelar"
-						width="100%"
-						variant="secondary"
-						onClick={close}
-						disabled={isLoading}
-					/>
-				</div>
-
-				<div class="button-box register-button">
-					<Button
-						text="Registrar"
-						width="100%"
-						onClick={handleRegisterButton}
-						loading={isLoading}
-					/>
-				</div>
-			</div>
+	<Modal onConfirm={handleRegisterButton} onClose={close} {isLoading} confirmButtonText="Registrar">
+		<div class="flex flex-col gap-6">
+			<p class="text-center text-lg font-medium sm:text-xl">
+				Digite o Id público do usuário que deseja registrar no evento
+			</p>
+			<FormInput
+				label={'Id Público'}
+				height="90px"
+				bind:value={publicUserIdInput}
+				onInput={(v) => {}}
+				isUppercase
+			/>
 		</div>
 	</Modal>
 {/if}
-
-<style>
-	.modal-container {
-		background: var(--modal-background-color);
-		box-shadow: 0 4px 4px 2px rgba(0, 0, 0, 0.5);
-	}
-
-	.buttons-box {
-		flex-direction: column-reverse;
-	}
-
-	.button-box {
-		width: 100%;
-	}
-
-	@media (min-width: 475px) {
-		.buttons-box {
-			flex-direction: row;
-		}
-
-		.cancel-button {
-			width: 130px;
-		}
-
-		.register-button {
-			width: 170px;
-		}
-	}
-</style>
