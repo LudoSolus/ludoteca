0db09e0d81ea4b34b7e78fc9cfffa849cb55a713
GustavinFromSdleks <gustavoseveriano@alunos.utfpr.edu.br>
2025-11-28 01:45:22 -0300

    ajuste e criacao do elemento de HomeEvents para o page da home

--- frontend/web/src/routes/user/+layout.ts
+++ frontend/web/src/routes/user/+layout.ts
@@ -8,20 +8,21 @@ import { decodeAuthJwt } from '$lib/shared/helpers/decode-jwt';
 import { EUserRole } from '$lib/shared/enums/user-role.enum';
 
 export const load: LayoutLoad = async () => {
-	if (!authService.isAuthenticated()) {
-		throw redirect(302, '/auth/login');
-	}
-
-	const tokenStore = authService.getUserToken();
-	const userAuthData = get(tokenStore) ? decodeAuthJwt(get(tokenStore)) : null;
-
-    if(!userAuthData){
+    if (!authService.isAuthenticated()) {
         throw redirect(302, '/auth/login');
     }
 
-    if(userAuthData.role == EUserRole.ADMIN){
+    const tokenStore = authService.getUserToken();
+    const userAuthData = get(tokenStore) ? decodeAuthJwt(get(tokenStore)) : null;
+
+    if (!userAuthData) {
+        throw redirect(302, '/auth/login');
+    }
+
+    if (userAuthData.role === EUserRole.ADMIN) {
         throw redirect(303, '/admin');
     }
 
-	return { userAuthData };
+    return { userAuthData };
 };
+
