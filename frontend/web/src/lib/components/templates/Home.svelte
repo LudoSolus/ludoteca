833bc570524ba08a6d77c0b767d1963806bd2928
Felipe Scalco <fscalco7@gmail.com>
2025-11-28 13:35:45 -0300

    tela do gustavo

---
+++ frontend/web/src/lib/components/templates/Home.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { IListNextEventsResponse } from '$lib/api/queries/events/list-next-events/list-next-events.interface';
+	import EventCard from '$lib/components/molecules/EventCard.svelte';
+
+	export let events: IListNextEventsResponse[];
+
+	function goToEvent(eventId: string): void {
+    console.log(eventId)
+		goto(`/user/event/${eventId}`);
+	}
+</script>
+
+<main class="flex w-full flex-col overflow-y-auto px-2 pt-7 pb-20 sm:px-4 md:px-10 xl:px-15">
+	<section class="mt-6 flex w-full flex-col gap-2 sm:mt-0 sm:gap-6">
+		<h3 class="h3">Próximos Eventos</h3>
+
+		<div class="flex w-full gap-8 overflow-x-scroll py-3">
+			{#each events as event}
+				<EventCard
+					name={event.name}
+					startDate={event.startDate}
+					endDate={event.finalDate}
+					address={{
+						street: event.street,
+						number: event.number,
+						neighborhood: event.neighborhood,
+						city: event.city,
+						state: event.state,
+						zipCode: event.zipCode
+					}}
+					onCLickButton={() => goToEvent(event.id)}
+					hasBoardGame
+					hasEscapeRoom
+					hasRpg
+				/>
+			{/each}
+		</div>
+	</section>
+</main>
--- frontend/web/src/routes/user/home/+page.svelte
+++ frontend/web/src/routes/user/home/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
-  import HomeEvents from "$lib/components/molecules/HomeEvents.svelte";
+	import type { IListNextEventsResponse } from '$lib/api/queries/events/list-next-events/list-next-events.interface';
+	import { ListNextEventsQuery } from '$lib/api/queries/events/list-next-events/list-next-events.query';
+	import Home from '$lib/components/templates/Home.svelte';
+	import { QueriesHandlerService } from '$lib/shared/handlers/query/queries-handler.service';
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+
+	const queriesHandler = new QueriesHandlerService(axios);
+	let events: IListNextEventsResponse[] = [];
+
+	onMount(() => {
+		fetchEvents();
+	});
+
+	function fetchEvents() {
+		queriesHandler.handle(new ListNextEventsQuery()).subscribe({
+			next: (res) => {
+				events = res.resultData;
+			},
+			error: (err) => {
+				console.log(err);
+			}
+		});
+	}
 </script>
 
-<main class="bg-[#fffcee] min-h-screen w-full px-6 py-10">
-  <div class="w-full max-w-[1600px] ml-0">
-    <HomeEvents />
-  </div>
-</main>
+<Home {events} />
