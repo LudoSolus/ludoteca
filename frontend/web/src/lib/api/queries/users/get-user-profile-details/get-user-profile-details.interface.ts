874fa718a5f8d97169448d64361a56a40395fb19
Felipe Scalco <fscalco7@gmail.com>
2025-11-29 19:16:09 -0300

    feat [WEB]: new queries

---
+++ frontend/web/src/lib/api/queries/users/get-user-profile-details/get-user-profile-details.interface.ts
@@ -0,0 +1,10 @@
+export interface IGetUserProfileDetailsResponse {
+	publicId: string;
+	name: string;
+	cpf: string;
+	instituitionId: string;
+	phone: string;
+	ra?: string;
+	email: string;
+	birthDate: Date;
+}
