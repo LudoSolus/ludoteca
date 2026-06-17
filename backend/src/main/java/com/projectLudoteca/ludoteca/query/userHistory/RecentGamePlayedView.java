a25057ee7491bcf6799d684bd8ad1e3cba0633ef
RaphaeldosSantosSouza <164585988+RaphaeldosSantosSouza@users.noreply.github.com>
2026-06-16 22:50:28 -0300

    Revert "234 feature relatório de jogos mais populares top games"

--- backend/src/main/java/com/projectLudoteca/ludoteca/query/userHistory/RecentGamePlayedView.java
+++
@@ -1,9 +0,0 @@
-package com.projectLudoteca.ludoteca.query.userHistory;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-public record RecentGamePlayedView(UUID gameId,
-                                   String gameName,
-                                   LocalDateTime playedAt) {
-}
